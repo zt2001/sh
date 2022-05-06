d04407d079e270171f6219d9789e4012dc977488
zt2001 <46849147+zt2001@users.noreply.github.com>
2022-05-06 13:34:57 +0800

    Update apollo.sh

--- apollo.sh
+++ apollo.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 #下载apollo3.0压缩包
-wget https://archive.fastgit.org/ApolloAuto/apollo/archive/refs/tags/v1.0.0.zip
+wget https://archive.fastgit.org/ApolloAuto/apollo/archive/refs/tags/v3.0.0.zip
 #解压
-unzip -o v1.0.0.zip
+unzip -o v3.0.0.zip
 #重命名剪贴到新路径
 mkdir work && mkdir work/ApolloAuto
-mv apollo-1.0.0 work/ApolloAuto/apollo
+mv apollo-3.0.0 work/ApolloAuto/apollo
 #删除压缩包
 
 read -p "是否删除压缩包(Y/N/其他):" value
 
 if [ "$value" == "Y" ] || [ "$value" == "y" ]; then
-	rm -rf v1.0.0.zip
+	rm -rf v3.0.0.zip
 	echo ""
 
 	echo "**********删除完毕**********"
