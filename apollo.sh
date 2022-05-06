33763f6e36ba89d1251a16012c4310d9a233c975
zt2001 <46849147+zt2001@users.noreply.github.com>
2022-05-06 11:08:26 +0800

    Create apollo.sh

---
+++ apollo.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+#下载apollo3.0压缩包
+wget https://archive.fastgit.org/ApolloAuto/apollo/archive/refs/tags/v1.0.0.zip
+#解压
+unzip -o v1.0.0.zip
+#重命名剪贴到新路径
+mkdir work && mkdir work/ApolloAuto
+mv apollo-1.0.0 work/ApolloAuto/apollo
+#删除压缩包
+
+read -p "是否删除压缩包(Y/N/其他):" value
+
+if [ "$value" == "Y" ] || [ "$value" == "y" ]; then
+	rm -rf v1.0.0.zip
+	echo ""
+
+	echo "**********删除完毕**********"
+	echo ""
+	echo "apollo下载脚本已运行完毕"
+	echo ""
+	echo "********sh.wyjs.ltd********"
+	echo ""
+	exit 0
+
+else
+	echo ""
+	echo "**********🌩**********"
+	echo ""
+	echo "apollo下载脚本已运行完毕"
+	echo ""
+	echo "******sh.wyjs.ltd******"
+	echo ""
+	exit 0
+fi
