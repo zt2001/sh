211a8a5eaf8fb13301bdc8e48c97cd7a93f40181
忘忧 <1694781739@qq.com>
2022-05-07 13:08:25 +0800

    添加菜单

--- main.sh
+++ main.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 sudo -v
 echo ""
-echo "**********编译完毕**********"
+echo "**********wyjs-sh**********"
+echo ''
 echo "1.编译apollo容器"
 echo "容器脚本已运行完毕"
 echo "2.进入apollo容器"
 echo "3.安装docker"
 echo "4.docker镜像加速"
+echo "5.下载apollo"
 echo "其他字符.退出"
 echo ''
+echo '************菜单************'
 read -p "请输入您的选择:" value
 if [ "$value" == "1" ]; then
 	cd
@@ -59,6 +62,45 @@ elif [ "$value" == "4" ]; then
 	sudo wget https://fastly.jsdelivr.net/gh/zt2001/sh@main/daemon.json
 	sudo systemctl daemon-reload
 	sudo systemctl restart docker
+	echo ""
+	echo "**********删除完毕**********"
+	echo ""
+	echo "容器脚本已运行完毕"
+	echo ""
+	echo "********sh.wyjs.ltd*********"
+	echo ""
+	pwd
+	exit 0
+
+elif [ "$value" == "5" ]; then
+	wget https://archive.fastgit.org/ApolloAuto/apollo/archive/refs/tags/v3.0.0.zip
+	#解压
+	unzip -o v3.0.0.zip
+	#重命名剪贴到新路径
+	mkdir work && mkdir work/ApolloAuto
+	mv apollo-3.0.0 work/ApolloAuto/apollo
+	#删除压缩包
+	read -p "是否删除压缩包(Y/N/其他):" value
+	if [ "$value" == "Y" ] || [ "$value" == "y" ]; then
+		rm -rf v3.0.0.zip
+		echo ""
+		echo "**********删除完毕**********"
+		echo ""
+		echo "apollo下载脚本已运行完毕"
+		echo ""
+		echo "*********sh.wyjs.ltd*********"
+		echo ""
+		exit 0
+	else
+		echo ""
+		echo "*************🌩*************"
+		echo ""
+		echo "apollo下载脚本已运行完毕"
+		echo ""
+		echo "*********sh.wyjs.ltd*********"
+		echo ""
+		exit 0
+	fi
 	echo ""
 	echo "**********删除完毕**********"
 	echo ""
