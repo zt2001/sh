64f614d69e95e2fa4cfd52fe3a5ca80557753886
忘忧 <1694781739@qq.com>
2022-05-09 20:45:02 +0800

    增加启动

---
+++ wyjs-sh.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+cd ~
+if [ ! -f "wyjs-sh/main.sh" ]; then
+tee -ai ~/.bashrc <<-'EOF'
+alias wyjs='bash ~/wyjs-sh/main.sh'
+EOF
+    cd;mkdir wyjs-sh;cd wyjs-sh;rm main.sh;wget https://cdn.staticaly.com/gh/zt2001/sh/main/main.sh ;chmod 777 main.sh;echo ""
+fi
+echo ''
+echo ''
+echo ''
+echo ''
+echo ''
+echo '**********************************************************'
+echo '*                                                        *'
+echo '*                  欢迎使用韬哥脚本                      *'
+echo '*                                                        *'
+echo "*             快捷指令 ' wyjs ' 来打开指令               *"
+echo '*                                                        *'
+echo "*  请重新打开终端或输入'source ~/.bashrc'后可执行该指令  *"
+echo '*                                                        *'
+echo '**********************************************************'
+echo ''
+echo ''
+echo ''
+echo ''
+echo ''
